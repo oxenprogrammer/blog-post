35b505207fcb431fae4061b0bf699cfb9e829b65
oxenprogrammer <emmy1000okello@gmail.com>
2021-02-17 23:43:35 +0300

    feat(blog): add single article show controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,9 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  # The read article
+  def show
+    @article = Article.find(params[:id])
+  end
 end
